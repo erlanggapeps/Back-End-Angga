09466b75fd40c69ceee006f97197f02eafc5c450
erlanggapeps <erlangga.prdn@gmail.com>
2024-07-27 10:41:50 +0700

    backend2

---
+++ rest-api/src/routers/user.router.ts
@@ -0,0 +1,10 @@
+import { Router }from 'express'
+import { createUser, getUserId, getUsers } from '../controllers/user.controller'
+
+const userRouter = Router()
+
+userRouter.get('/', getUsers)
+userRouter.get('/:id',getUserId)
+userRouter.post('/', createUser)
+
+export { userRouter }
